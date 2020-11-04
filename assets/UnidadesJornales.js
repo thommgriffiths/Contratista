f0113627612166719c23b8e24ca5fdbfb97ef1af
Thomas Griffiths <chomas.g@hotmail.com>
2020-11-04 16:59:30 -0300

    limpieza pre presentacion

--- assets/UnidadesJornales.js
+++ assets/UnidadesJornales.js
@@ -1,83 +1,119 @@
 const UNIJornales = [
   {
-    label: "0,5",
+    label: "1",
     valor: "1",
   },
   {
-    label: "1",
+    label: "2",
     valor: "2",
   },
   {
-    label: "1,5",
+    label: "3",
     valor: "3",
   },
   {
-    label: "2",
+    label: "4",
     valor: "4",
   },
   {
-    label: "2,5",
+    label: "5",
     valor: "5",
   },
   {
-    label: "3",
+    label: "6",
     valor: "6",
   },
   {
-    label: "3,5",
+    label: "7",
     valor: "7",
   },
   {
-    label: "4",
+    label: "8",
     valor: "8",
   },
   {
-    label: "4,5",
+    label: "9",
     valor: "9",
   },
   {
-    label: "5",
+    label: "10",
     valor: "10",
   },
   {
-    label: "5,5",
+    label: "11",
     valor: "11",
   },
   {
-    label: "6",
+    label: "12",
     valor: "12",
   },
   {
-    label: "6,5",
+    label: "13",
     valor: "13",
   },
   {
-    label: "7",
+    label: "14",
     valor: "14",
   },
   {
-    label: "7,5",
+    label: "15",
     valor: "15",
   },
   {
-    label: "8",
+    label: "16",
     valor: "16",
   },
   {
-    label: "8,5",
+    label: "17",
     valor: "17",
   },
   {
-    label: "9",
+    label: "18",
     valor: "18",
   },
   {
-    label: "9,5",
+    label: "19",
     valor: "19",
   },
   {
-    label: "10",
+    label: "20",
     valor: "20",
+  },
+  {
+    label: "21",
+    valor: "21",
+  },
+  {
+    label: "22",
+    valor: "22",
+  },
+  {
+    label: "23",
+    valor: "23",
+  },
+  {
+    label: "24",
+    valor: "24",
+  },
+  {
+    label: "25",
+    valor: "25",
+  },
+  {
+    label: "26",
+    valor: "26",
+  },
+  {
+    label: "27",
+    valor: "27",
+  },
+  {
+    label: "28",
+    valor: "28",
+  },
+  {
+    label: "29",
+    valor: "29",
   },
 ];
 
---
+++ assets/userID.jsx
@@ -0,0 +1,5 @@
+const LoggedUser = {
+  id: "702c4c62-ac75-4642-a8cd-f9dc1be2427e",
+};
+
+export default LoggedUser;
